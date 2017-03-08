78cb1b44fc3c6fe1e992484ebe4d94e630d1fa3e
Andrew Au <andrew.au@cbc.ca>
2017-03-07 22:13:59 -0500

    add alpha validator to text fields

--- clubmanneremail/emailsubmission/models.py
+++ clubmanneremail/emailsubmission/models.py
@@ -4,12 +4,11 @@ from django.forms import ModelForm
 
 # Create your models here.
 class Emails(models.Model):
-    first_name = models.CharField(max_length=50, blank=False, null=False)
-    last_name = models.CharField(max_length=50, blank=False, null=False)
+    alphabetic = RegexValidator(r'^[a-zA-Z]*$', 'Only alphabetic characters are allowed.')
+    first_name = models.CharField(max_length=50, blank=False, null=False, validators=[alphabetic])
+    last_name = models.CharField(max_length=50, blank=False, null=False, validators=[alphabetic])
     email = models.EmailField(max_length=254, blank=False, null=False, unique=True)
-    def __str__(self):              # __unicode__ on Python 2
-        return self.first_name
-
+    
     class Meta:
         managed = True
         db_table = 'emails'
